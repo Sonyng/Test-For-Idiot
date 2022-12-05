678792803a2fc617ad55f8a21f926670e5e0bdd9
Vinicius Guimarães <110570340+Sonyng@users.noreply.github.com>
2022-12-04 22:23:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+var buttton = document.getElementById("SIM")
+var buttton2 = document.getElementById("NAO")
+
+buttton.onclick = function() {
+    alert("EU SABIA KKKKKKKKKKKKKKKK")
+}
+
+buttton2.addEventListener("mouseover", function() {
+    this.style.top = "30%";
+    this.style.left = "20%";
+    
+    buttton2.addEventListener("mouseover", function() {
+        this.style.top = "70%";
+        this.style.left = "40%";
+        buttton2.addEventListener("mouseover", function() {
+            this.style.top = "10%";
+            this.style.left = "60%";
+            buttton2.addEventListener("mouseover", function() {
+                this.style.top = "30%";
+                this.style.left = "90%";
+                buttton2.addEventListener("mouseover", function() {
+                    this.style.top = "60%";
+                    this.style.left = "40%";
+
+                    buttton2.addEventListener("mouseover", function() {
+                        this.style.top = "80%";
+                        this.style.left = "60%";
+                        buttton2.addEventListener("mouseover", function() {
+                            this.style.top = "50%";
+                            this.style.left = "10%";
+
+                            buttton2.addEventListener("mouseover", function() {
+                                this.style.top = "20%";
+                                this.style.left = "37%";
+                                buttton2.addEventListener("mouseover", function() {
+                                    document.location.reload(true);
+                                    buttton2.addEventListener("mouseover", function() {
+                                        this.style.top = "30%";
+                                        this.style.left = "80%";
+                                        buttton2.addEventListener("mouseover", function() {
+                                            buttton2.addEventListener("mouseover", function() {
+                                                this.style.top = "60%";
+                                                this.style.left = "40%";
+                                                buttton2.addEventListener("mouseover", function() {
+                                                    this.style.top = "30%";
+                                                    this.style.left = "90%";
+                                                    buttton2.addEventListener("mouseover", function() {
+                                                    document.location.reload(true);
+                                                buttton2.addEventListener("mouseover", function() {
+                                                    this.style.top = "90%";
+                                                    this.style.left = "50%";
+                                                    buttton2.addEventListener("mouseover", function() {
+                                                    this.style.top = "60%";
+                                                    this.style.left = "50%";
+                                                    buttton2.addEventListener("mouseover", function() {
+                                                        this.style.top = "70%";
+                                                        this.style.left = "20%";
+                                                        buttton2.addEventListener("mouseover", function() {
+                                                            document.location.reload(true);
+                                                
+                                                
+   
+})
+})
+})
+})     
+})
+})
+})     
+})
+})
+})
+})
+})
+})
+})
+})
+})
+})
+})
